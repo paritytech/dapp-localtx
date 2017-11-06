6bf921878ab2bcbb2c0a1c5fd228df93c61db91a
Jaco Greeff <jaco@parity.io>
2017-11-06 12:09:17 +0100

    Rebuild with updated dependencies

--- src/Application/application.spec.js
+++ src/Application/application.spec.js
@@ -17,7 +17,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import '@parity/shared/environment/tests';
+import '@parity/shared/lib/environment/tests';
 
 import Application from './application';
 
--- src/Transaction/transaction.spec.js
+++ src/Transaction/transaction.spec.js
@@ -18,7 +18,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import EthApi from '@parity/api';
-import '@parity/shared/environment/tests';
+import '@parity/shared/lib/environment/tests';
 
 // Mock API for tests
 import * as Api from '../parity';
